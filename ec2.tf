1b5d2ea86da2158de46c245d84836111407b3c5a
Marcos Junior <juninho1_2010@hotmail.com>
2023-07-19 19:42:08 -0300

    add: EC2 user data with nginx and security group

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,15 @@
 resource "aws_instance" "web" {
   ami = var.image_id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+    service nginx start
+  EOF
 
   tags = {
-    Name = "Laboratório Devops"
+    Name = "Laboratório Devops.V2"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "image_id" {
   default = "ami-0b93ce03dcbcb10f6"
 }
+
+variable "sg_name"{
+  default = "my-sg"
+}
