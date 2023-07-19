2ca9d2b24ac4011010f0286980387e58ee5bccb9
Marcos Junior <juninho1_2010@hotmail.com>
2023-07-19 19:45:46 -0300

    change security group name

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami = var.image_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.example_sg.id]
+  vpc_security_group_ids = [aws_security_group.var.sg_name.id]
   user_data = <<-EOF
     #!/bin/bash
     apt-get update
