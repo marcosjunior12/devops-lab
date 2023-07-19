8e140bccc391a43fd317cdc2e85a9cccc5acb054
Marcos Junior <juninho1_2010@hotmail.com>
2023-07-19 19:51:51 -0300

    fix: creating security group

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,26 @@
+resource "aws_security_group" "my-sg" {
+  name        = var.sg_name
+  description = "Grupo de segurança permitindo conexões HTTP e HTTPS"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami = var.image_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.var.sg_name.id]
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
   user_data = <<-EOF
     #!/bin/bash
     apt-get update
